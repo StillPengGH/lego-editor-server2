da6ff38545141fe346b460cf1345452ff860f35f
stillpeng <stillpeng_git@163.com>
2021-11-12 15:04:29 +0800

    change project dir

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(bodyparser({
 }))
 app.use(json())
 app.use(logger())
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(require('koa-static')(__dirname + '/src/public'))
 
 app.use(views(__dirname + '/views', {
   extension: 'pug'
