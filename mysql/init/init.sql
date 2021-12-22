4af5e0d36205a6b2a1be2db29136a04e6c9c3a31
stillpeng <stillpeng_git@163.com>
2021-12-22 15:45:55 +0800

    chore: edit yml for deploy test

--- mysql/init/init.sql
+++ mysql/init/init.sql
@@ -4,10 +4,12 @@ select "init start...";
 
 -- 设置 root 用户可外网访问
 use mysql;
-SET SQL_SAFE_UPDATES=0; -- 解除安全模式，测试环境，没关系
+-- 解除安全模式，测试环境，没关系
+SET SQL_SAFE_UPDATES=0; 
 update user set host='%' where user='root';
 flush privileges;
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY ''; -- 密码参考 docker-compose.yml
+-- 密码参考 docker-compose.yml
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '123456'; 
 flush privileges;
 
 select "init end...";
